fb70a0ddf3c9325ea1613055868185889d924229
lalo.com <lalo@lalo.com>
2018-06-13 15:55:19 +0000

    Actualizacion guardado local

--- python-firebase.py
+++ python-firebase.py
@@ -3,25 +3,17 @@ from sense_hat import SenseHat
 import time
 import random
 from firebase import firebase
+import datetime
 
 sense = SenseHat()
 
-'''def temp():
-	temp = round(sense.get_temperature(), 1)
-
-def hum():
-	hum = round(sense.get_humidity(), 1)
-
-def press():
-	pres = round(sense.get_pressure(), 1)'''
-
 firebase = firebase.FirebaseApplication("https://estacionmeteorologicaverano.firebaseio.com", None)
-data='00'
-i = 0
-j = 0
-while True:
-	i+=30
-	j+=1
+file = open('data.txt', 'w')
+time.sleep(13)
+sense.clear(0, 240, 21)
+time.sleep(2)
+sense.clear(0, 0, 0)
+for i in range(1020):
 	temp = round(sense.get_temperature(), 1)
 	hum = round(sense.get_humidity(), 1)
 	pres = round(sense.get_pressure(), 1)
@@ -29,26 +21,21 @@ while True:
 	mag = sense.get_compass_raw()
 	acc = sense.get_accelerometer_raw()
 	gyro = sense.get_gyroscope_raw()
-	data = {"Temp":temp,
-			'Hum':hum,
-			'Pres':pres,
-			'Orient':ori,
-			'Mag':mag,
-			'Acc':acc,
-			'Gyro':gyro}
-	print(data)
-	salida = firebase.post('/registros2Test', data)
-	sense.show_message("Medición {}".format(j), scroll_speed=0.08)
+	data = {
+			'temperature':temp,
+			'humidity':hum,
+			'pressure':pres,
+			'orientation':ori,
+			'compass':mag,
+			'acceleration':acc,
+			'gyroscope':gyro,
+			'time': datetime.datetime.now().isoformat()
+			}
+	try:
+		firebase.post('/registros-v3', data)
+	except:
+		file.write(str(data) + ',\n')
 	
-	time.sleep(30)
-	if i > 600 :
-		break
-#dataInJson = json.loads(data)
-#firebase.post("/registros", data)
-
-salida = firebase.get('/registros', None)
-'''for key in salida.keys():
-	registro = salida[key]
-	print(registro['calificacion'])
-	print(registro['edad'])'''
-#print(salida)
+	time.sleep(1)
+sense.clear(255, 0, 0)
+file.close()
